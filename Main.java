cafc2a535776b803527b1260a3e3084b45e76d29
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-02-25 18:56:02 +0000

    Added int variable.

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 class Main{
   public static void main(String[]args){
    double number=20;
+    int number2=10;
 
     //We can add or subtract using number= number+5,number = number-5,or number += 5, number -=5. 
     number +=5;
+    number2 -=5;
     System.out.println(number); 
+    System.out.println(number2);
   }
 }
