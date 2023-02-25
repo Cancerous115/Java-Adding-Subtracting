5040f141c66c8dca633d36fab35208f0089b1c9e
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-02-25 18:52:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main{
+  public static void main(String[]args){
+   double number=20;
+
+    //We can add or subtract using number= number+5,number = number-5,or number += 5, number -=5. 
+    number +=5;
+    System.out.println(number); 
+  }
+}
